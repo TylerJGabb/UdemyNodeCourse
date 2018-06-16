be59e203b2b10c9b516ab7252c36071e4f0ce6e8
tylerjgabb <tylerjgabb@gmail.com>
2018-06-16 10:12:53 -0700

    added config package

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,4 @@
+const config = require('config')
 const helmet = require('helmet');
 const Joi = require('joi')
 const express = require('express');
@@ -8,6 +9,12 @@ app.use(express.json());
 app.use(express.urlencoded( {extended : true}));
 app.use(express.static('public')); //localhost:3000/readme.txt
 app.use(helmet());
+
+//Configuration
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
+console.log('Mail Server: ' + config.get('mail.password'));
+
 
 //process.env.NODE_ENV
 if(app.get('env') === 'development'){
