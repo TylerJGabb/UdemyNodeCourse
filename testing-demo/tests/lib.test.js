ed844bc9568a23dc57f28e6e5e6517f3d702f9eb
tylerjgabb <tylerjgabb@gmail.com>
2018-07-09 21:08:55 -0700

    added regex matching example, testing strings

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -16,3 +16,10 @@ describe('absolute', () => {
     expect(result).toBe(0);
   });
 });
+
+describe('greet', () => {
+  it('should return the greeting message', () => {
+    const result = lib.greet('Tyler');
+    expect(result).toMatch(/Tyler/);
+  })
+});
